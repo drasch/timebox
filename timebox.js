130c03fea3021a8da20f4e7fb59f21607c24d60c
David Rasch <rasch@raschnet.com>
2024-02-12 11:50:58 -0500

    fix(timer): dark mode

--- timebox.js
+++ timebox.js
@@ -41,14 +41,19 @@ function draw() {
   strokeWeight(0);
   arc(windowWidth/2.0,windowHeight/2.0,w,w, min(arcLength, 3*HALF_PI-0.01), 3 * HALF_PI, PIE);
 
-  stroke('black');
-  strokeWeight(1);
+  stroke(222);
   for (let i = 0; i < 12; i++) {
+    let startRad = i % 3 == 0 ? 0.75 : 0.8;
+    if (i % 3 == 0 ) {
+      strokeWeight(4);
+    } else {
+      strokeWeight(1);
+    }
     let r = w/2.0;
     let cx = windowWidth/2.0;
     let cy = windowHeight/2.0;
     let rads = 2*PI*i/12.0 - HALF_PI;
-    line(cx + 0.8*r*cos(rads), cy + 0.8*r*sin(rads), cx + 0.9*r*cos(rads), cy + 0.9*r*sin(rads));
+    line(cx + startRad*r*cos(rads), cy + startRad*r*sin(rads), cx + 0.9*r*cos(rads), cy + 0.9*r*sin(rads));
   }
 
   if (time <0 ){
