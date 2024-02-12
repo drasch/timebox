82002b957e6b67f655a6d91082327cb4cc84883c
David Rasch <rasch@raschnet.com>
2024-02-12 09:55:41 -0500

    feat(timer): add real timer functionality, assumes 60m for now

--- timebox.js
+++ timebox.js
@@ -1,12 +1,13 @@
-let time=60;
+let setTime=60;
 let c;
 let w;
+let startTime=new Date();
 
+let white=245;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 
-  background(300);
+  background(white);
   frameRate(30);
 
   canvasSetup();
@@ -23,22 +24,23 @@ function canvasSetup() {
   text('30', windowWidth/2.0, windowHeight/2.0 + w/2.0 + 20);
   text('45', windowWidth/2.0 + w/2.0 + 20, windowHeight/2.0);
   text('60', windowWidth/2.0, windowHeight/2.0 - w/2.0 - 20);
-  stroke('white');
+  stroke(white);
 
 }
 
 function draw() {
+  let time = setTime - (new Date() - startTime)/(1000.0*60);
   let arcLength = (60-time)/60.0 * 2* PI - HALF_PI;
-  
-  stroke('white');
-  fill('white');
+
+  stroke(white);
+  fill(white);
   strokeWeight(1.0);
   arc(windowWidth/2.0,windowHeight/2.0,w,w, 0, 2*PI, PIE);
 
   fill('red');
   strokeWeight(0);
   arc(windowWidth/2.0,windowHeight/2.0,w,w, min(arcLength, 3*HALF_PI-0.01), 3 * HALF_PI, PIE);
-  
+
   stroke('black');
   strokeWeight(1);
   for (let i = 0; i < 12; i++) {
@@ -49,18 +51,17 @@ function draw() {
     line(cx + 0.8*r*cos(rads), cy + 0.8*r*sin(rads), cx + 0.9*r*cos(rads), cy + 0.9*r*sin(rads));
   }
 
-  time -= 0.01;
   if (time <0 ){
     noLoop();
   }
 }
 
 function mousePressed() {
-  time=60;
+  startTime=new Date();
   loop();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  canvasSetup(); 
+  canvasSetup();
 }
