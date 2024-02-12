4b1ff6191f49ff01915e10a0b4654d1a3c23bbfa
David Rasch <rasch@raschnet.com>
2024-02-12 09:54:51 -0500

    feat(clock): initial clock

---
+++ clock.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  frameRate(5);
+
+  font = loadFont('ttf/Monocraft.otf');
+  textFont(font);
+  canvasSetup();
+}
+
+function canvasSetup() {
+  background(20);
+  w = min(windowWidth * 0.2 ,windowHeight * 0.4);
+  textSize(w);
+}
+
+function draw() {
+  let time = String(hour()).padStart(2,'0') + ":" + String(minute()).padStart(2,'0')
+
+  stroke('red');
+  fill('red');
+  textAlign(CENTER, CENTER);
+  strokeWeight(1);
+  text(time, windowWidth/2.0, windowHeight/2.0);
+}
+
+function mousePressed() {
+  startTime=new Date();
+  loop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  canvasSetup();
+}
